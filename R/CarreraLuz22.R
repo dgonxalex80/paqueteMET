c4ddc85880474c2ba7b70c80f7f51e9754f37313
Daniel E Gonzalez <dgonxalex80@gmail.com>
2022-12-28 18:41:11 -0500

    ajustes documentacion del paqueteMET

---
+++ R/CarreraLuz22.R
@@ -0,0 +1,16 @@
+#' @title Tiempos carrera de la Luz Cali 2022
+#'
+#' @description Información sobre los tiempo de la carrera de La Luz Cali 2022.
+#'
+#' @format Una base de dstos con  1922 fila y 7 variables:
+#' \describe{
+#'   \item{id}{número identificador del corredor.}
+#'   \item{sex}{sexo del corredor.}
+#'   \item{edad}{edad  del corredor.}   
+#'   \item{origen}{lugar de procedencia.}
+#'   \item{timerun}{tiempo de carrera en segundos.}
+#'   \item{categoria}{categoría del corredor segun su edad.}   
+#'   \item{nacionalidad}{nacionalidad del corredor.}
+#' }
+#' @source <https://www.athlinks.com/event/212331/results/Event/1031677/Results>
+"CarreraLuz2022"
